91c477ff032d6409494ecb919a2066fd3ca3a3c6
JGrineau <jgrineau95@gmail.com>
2023-07-23 14:47:58 -0400

    CSS pdates for the Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AtricleListComponent } from './atricle-list/atricle-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: AtricleListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: AtricleListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
